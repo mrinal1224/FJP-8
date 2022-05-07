f58a2b6a2a6ca1077a6301ff9fa0f9a5cbc56ed5
mrinal1224 <iammrinalkc@gmail.com>
2022-05-07 21:44:57 +0530

    variables

---
+++ 2_JavaScript/variables.js
@@ -0,0 +1,61 @@
+// var a = 12.34 // Dynamic Typing
+
+// console.log(a)
+
+
+// var a = 'my name is Steve'
+
+// // Var keyword's problem is that it can be Redclared
+// //Solution - by using let we can solve this problem
+
+// console.log(a)
+// // Var can be reassigned
+// a = 'Pepcoding'
+
+// console.log(a)
+// var b = true
+// var c = 230000
+
+
+// let keyword
+
+// let d = 'Pepcoding'
+
+// console.log(d)
+
+// d = 23
+
+// console.log(d)
+
+
+
+// var is function Scoped and let is Block Scoped
+
+// function test(){
+//        var a = 2
+//        console.log(a) // 2
+// }
+// test()
+// console.log(a) // this is not allowed 
+
+
+// if(true){
+//     var b = 4
+//     console.log(b) // 4  
+// }
+
+
+// console.log(b) // downside of using var 
+// Solution - use let to avoid scope of var
+
+if(true){
+       let c = 'pep'
+       console.log(c)
+} // this is block
+
+console.log(c)
+
+
+
+
+
