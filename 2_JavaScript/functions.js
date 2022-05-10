794cd54e6592100cb15df4afe5a95c42adbf0c54
mrinal1224 <iammrinalkc@gmail.com>
2022-05-10 22:59:31 +0530

    push

---
+++ 2_JavaScript/functions.js
@@ -0,0 +1,65 @@
+// Normal function
+
+
+
+// sayhi()// function Invokation
+
+// parmeters
+// function add(c, d) {
+//   console.log(c + d);
+// }
+
+// function multipication(c, d) {
+//   console.log(c * d);
+// }
+
+// function substraction(c, d) {
+//   console.log(c - d);
+// }
+
+function division(c, d) {
+  console.log(c/d);
+}
+
+// // multipication , substraction , division
+
+// add(3, 4); // Arguments
+// substraction(4 , 2)
+// division(10,2)
+// multipication(4 , 5)
+
+
+
+// function as expressions
+
+
+
+// function sayhi(){
+//     console.log('Hi Pepcoders')
+// } // function Defination
+
+
+// function Expression
+// let sayHi = function(){
+//        console.log('Hi')
+// }
+
+// sayHi()
+
+// IIFE (Immediately Invoked Function expresion)
+
+
+// let substract = (function(a , b){
+//        console.log(a-b)
+// }(20 , 10))
+
+
+
+function test(a , b){
+       return a+b
+}
+
+
+let addtion = test(4 , 5)
+
+console.log(addtion)
