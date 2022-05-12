6f4eabfa621d710cfac294bbbaf4c3bb2d179e0b
mrinal1224 <iammrinalkc@gmail.com>
2022-05-12 23:10:47 +0530

    path

---
+++ 3_NodeJS/nodePackages/path.js
@@ -0,0 +1,19 @@
+const path = require('path')
+
+
+let basename = path.basename('D:\\Batches\\FJP-8\\3_NodeJS\\nodePackages\\f3.txt')
+console.log('BaseName->'+ basename)
+
+let ext = path.extname('D:\\Batches\\FJP-8\\3_NodeJS\\nodePackages\\f3.txt')
+console.log(ext)
+
+
+
+console.log(__dirname)
+console.log(__filename)
+
+
+
+
+
+
