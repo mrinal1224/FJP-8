d4571afd837fae1d3964098d20ad861a9eb0dd5a
mrinal1224 <iammrinalkc@gmail.com>
2022-06-03 21:40:14 +0530

    calllbacks

---
+++ JS Concepts/callback.js
@@ -0,0 +1,26 @@
+//callback - A function that can be passed to another
+//function as an argumenet so that it can be called later is known as a callback function
+
+function printFirstName(firstName, cb) {
+  console.log(firstName);
+  cb("jha");
+}
+
+// function printLastName(lastName){
+//        console.log(lastName)
+// }
+
+printFirstName("Neeraj", function printLastName(lastName) {
+  console.log(lastName);
+});
+
+function printHello(cb) {
+  console.log("Hello");
+  cb();
+}
+
+function printBye() {
+  console.log("Bye");
+}
+
+printHello(printBye);
