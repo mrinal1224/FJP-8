9bc52aee8617b6ce3ab40416b8c76937ff380014
mrinal1224 <iammrinalkc@gmail.com>
2022-06-04 22:48:03 +0530

    push

--- JS Concepts/higher Order Functions/every.js
+++ JS Concepts/higher Order Functions/every.js
@@ -1,3 +1,5 @@
+// find: Conditon Check: To get true or false based on a condition 
+//it will return true if every element satisfies the condition -
 const transactions = [1000, 3000, 4000, 2000, 898, 3800,4500];
 
 
