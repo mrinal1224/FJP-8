b6eb36248319bf819e5c69b416640cb2e784620d
mrinal1224 <iammrinalkc@gmail.com>
2022-06-12 22:59:27 +0530

    TICKET GENERATE

--- Browser/Project-Ticket Mangement/script.js
+++ Browser/Project-Ticket Mangement/script.js
@@ -1,5 +1,7 @@
 let addBtn = document.querySelector(".add-btn");
 let modalCont = document.querySelector(".modal-cont");
+let mainCont = document.querySelector('.main-cont')
+let taskArea = document.querySelector('.textArea-cont')
 
 let addFlag = false;
 
@@ -14,3 +16,34 @@ addBtn.addEventListener("click", function () {
     modalCont.style.display = "none";
   }
 });
+
+
+
+modalCont.addEventListener("keydown" , function(e){
+       let key = e.key
+
+       if(key=='Shift'){
+              createTicket(taskArea.value)
+              modalCont.style.display='none'
+              addFlag=false
+              taskArea.value =''
+       }
+})
+
+
+function createTicket(ticketTask){
+     let ticketCont = document.createElement('div')
+     ticketCont.setAttribute('class' , 'ticket-cont')
+
+     ticketCont.innerHTML = `<div class="ticket-color"></div>
+     <div class="ticket-id">id 1</div>
+     <div class="task-area">${ticketTask}</div>
+     <div class="ticket-lock">
+       <i class="fa-solid fa-lock"></i>
+    </div>`
+
+
+    mainCont.append(ticketCont)
+
+
+}
