f425e842734fb602a12018492bfe3694751d0885
mrinal1224 <iammrinalkc@gmail.com>
2022-06-17 21:27:07 +0530

    ui removal

--- Browser/Project-Ticket Mangement/script.js
+++ Browser/Project-Ticket Mangement/script.js
@@ -1,9 +1,12 @@
 let addBtn = document.querySelector(".add-btn");
 let modalCont = document.querySelector(".modal-cont");
-let mainCont = document.querySelector('.main-cont')
-let taskArea = document.querySelector('.textArea-cont')
+let mainCont = document.querySelector(".main-cont");
+let taskArea = document.querySelector(".textArea-cont");
+
+let removeBtn = document.querySelector(".remove-btn");
 
 let addFlag = false;
+let removeFlag = false;
 
 addBtn.addEventListener("click", function () {
   // display The Modal
@@ -17,33 +20,47 @@ addBtn.addEventListener("click", function () {
   }
 });
 
+modalCont.addEventListener("keydown", function (e) {
+  let key = e.key;
 
+  if (key == "Shift") {
+    createTicket(taskArea.value);
+    modalCont.style.display = "none";
+    addFlag = false;
+    taskArea.value = "";
+  }
+});
 
-modalCont.addEventListener("keydown" , function(e){
-       let key = e.key
+function createTicket(ticketTask) {
+  let id = shortid();
+  let ticketCont = document.createElement("div");
+  ticketCont.setAttribute("class", "ticket-cont");
 
-       if(key=='Shift'){
-              createTicket(taskArea.value)
-              modalCont.style.display='none'
-              addFlag=false
-              taskArea.value =''
-       }
-})
-
-
-function createTicket(ticketTask){
-     let ticketCont = document.createElement('div')
-     ticketCont.setAttribute('class' , 'ticket-cont')
-
-     ticketCont.innerHTML = `<div class="ticket-color"></div>
-     <div class="ticket-id">id 1</div>
+  ticketCont.innerHTML = `<div class="ticket-color"></div>
+     <div class="ticket-id">#${id}</div>
      <div class="task-area">${ticketTask}</div>
      <div class="ticket-lock">
        <i class="fa-solid fa-lock"></i>
-    </div>`
+    </div>`;
 
+  mainCont.append(ticketCont);
 
-    mainCont.append(ticketCont)
+  handleRemoval(ticketCont);
+}
 
+removeBtn.addEventListener("click", function () {
+  removeFlag = !removeFlag;
 
+  if (removeFlag === true) {
+    removeBtn.style.color = "red";
+  } else {
+    removeBtn.style.color = "white";
+  }
+});
+
+function handleRemoval(ticket) {
+  ticket.addEventListener("click", function () {
+    if (!removeFlag) return;
+    ticket.remove(); // ui Removal
+  });
 }
