78e5a5caf0ab7744179e54c6b3bd458aa8cb044d
mrinal1224 <iammrinalkc@gmail.com>
2022-07-01 22:56:36 +0530

    todo

--- React/todo/src/components/Todo.js
+++ React/todo/src/components/Todo.js
@@ -3,11 +3,41 @@ import React, { Component } from 'react'
 export class Todo extends Component {
 
 
+       constructor(){
+              super()
+
+              this.state ={
+                     tasks:[],
+                     currTask : ''
+              }
+       }
+
+
+       handleChange =(e)=>{
+              console.log(e.target.value)
+              this.setState({
+                    currTask :e.target.value
+              })
+       }
+
+
+
+       handleAddTask = ()=>{
+            this.setState({
+               tasks : [...this.state.tasks , {task:this.state.currTask}]
+            })
+
+
+            
+       }
+
+
 
   render() {
     return (
       <div>
-       <h1>MY TodoList</h1>
+       <input type='text' onChange={this.handleChange}/>
+       <button onClick={this.handleAddTask}>Add Task</button>
       </div>
     )
   }
