05c88f969a6f19220752d3fc2b63a1c1a8164f39
mrinal1224 <iammrinalkc@gmail.com>
2022-07-01 22:25:59 +0530

    react todo

---
+++ React/todo/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Todo from './components/Todo'
+
+function App() {
+  return (
+    <div>
+      <Todo/>
+    </div>
+  );
+}
+
+export default App;
---
+++ React/todo/src/components/Todo.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+export class Todo extends Component {
+
+
+
+  render() {
+    return (
+      <div>
+       <h1>MY TodoList</h1>
+      </div>
+    )
+  }
+}
+
+export default Todo
