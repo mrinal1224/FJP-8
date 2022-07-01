8eec4faa5e8929fa4a62657a679074aea2eb8780
mrinal1224 <iammrinalkc@gmail.com>
2022-07-01 23:17:32 +0530

    add task

--- React/todo/src/components/Todo.js
+++ React/todo/src/components/Todo.js
@@ -1,46 +1,45 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export class Todo extends Component {
+  constructor() {
+    super();
 
+    this.state = {
+      tasks: [],
+      currTask: "",
+    };
+  }
 
-       constructor(){
-              super()
+  handleChange = (e) => {
+    console.log(e.target.value);
+    this.setState({
+      currTask: e.target.value,
+    });
+  };
 
-              this.state ={
-                     tasks:[],
-                     currTask : ''
-              }
-       }
-
-
-       handleChange =(e)=>{
-              console.log(e.target.value)
-              this.setState({
-                    currTask :e.target.value
-              })
-       }
-
-
-
-       handleAddTask = ()=>{
-            this.setState({
-               tasks : [...this.state.tasks , {task:this.state.currTask}]
-            })
-
-
-            
-       }
-
-
+  handleAddTask = () => {
+    this.setState({
+      tasks: [...this.state.tasks, { task: this.state.currTask }],
+    });
+  };
 
   render() {
     return (
       <div>
-       <input type='text' onChange={this.handleChange}/>
-       <button onClick={this.handleAddTask}>Add Task</button>
+        <input type="text" onChange={this.handleChange} />
+        <button onClick={this.handleAddTask}>Add Task</button>
+
+
+        <ul>
+              {this.state.tasks.map((taskObj) =>(
+                      <li>
+                            <p>{taskObj.task}</p>
+                     </li>
+              ))}
+        </ul>
       </div>
-    )
+    );
   }
 }
 
-export default Todo
+export default Todo;
