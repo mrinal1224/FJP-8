9470a3d4095f2a7e03d826dd465fa3e4c385e46f
mrinal1224 <iammrinalkc@gmail.com>
2022-07-02 22:12:20 +0530

    todo

--- React/todo/src/components/Todo.js
+++ React/todo/src/components/Todo.js
@@ -12,30 +12,56 @@ export class Todo extends Component {
 
   handleChange = (e) => {
     console.log(e.target.value);
+
     this.setState({
       currTask: e.target.value,
     });
-  };
+  }; // This function will get the value of your task from input field
 
   handleAddTask = () => {
     this.setState({
-      tasks: [...this.state.tasks, { task: this.state.currTask }],
+      tasks: [
+        ...this.state.tasks,
+        { task: this.state.currTask, id: this.state.tasks.length + 1 },
+      ],
+    });
+
+    this.setState({
+      currTask: "",
+    });
+  };
+
+  handleDelete = (id) => {
+    let narr = this.state.tasks.filter((taskObj) => {
+      return taskObj.id != id;
+    });
+
+    this.setState({
+      tasks: [...narr],
     });
   };
 
   render() {
     return (
       <div>
-        <input type="text" onChange={this.handleChange} />
+        <input
+          type="text"
+          value={this.state.currTask}
+          onChange={this.handleChange}
+        />
         <button onClick={this.handleAddTask}>Add Task</button>
 
-
         <ul>
-              {this.state.tasks.map((taskObj) =>(
-                      <li>
-                            <p>{taskObj.task}</p>
-                     </li>
-              ))}
+          {this.state.tasks.map((taskObj) => {
+            return (
+              <li>
+                <p>{taskObj.task}</p>
+                <button onClick={() => this.handleDelete(taskObj.id)}>
+                  Delete
+                </button>
+              </li>
+            );
+          })}
         </ul>
       </div>
     );
