3a9f5b5b76efe6062e1658c2f3d5fadcdb16c869
mrinal1224 <iammrinalkc@gmail.com>
2022-07-04 22:58:38 +0530

    push

--- React/movieapp/src/App.js
+++ React/movieapp/src/App.js
@@ -1,6 +1,7 @@
 import './App.css'
 
 import Banner from "./components/Banner";
+import MovieList from './components/MovieList';
 import Navbar from "./components/Navbar";
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
    <>
      <Navbar/>
      <Banner/>
+     <MovieList/>
    </>
   );
 }
---
+++ React/movieapp/src/components/MovieList.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+
+export class MovieList extends Component {
+  render() {
+    return (
+      <>
+      
+        <div>
+          <h3 className="text-center">
+            <strong>Trending</strong>
+          </h3>
+        </div>
+
+
+        <div className="movies-list">
+              
+        </div>
+      </>
+    );
+  }
+}
+
+export default MovieList;
