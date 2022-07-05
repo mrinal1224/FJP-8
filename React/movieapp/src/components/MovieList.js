1dd95c2941313a62fabcd95f76696ef6bab87411
mrinal1224 <iammrinalkc@gmail.com>
2022-07-05 21:36:39 +0530

    push

--- React/movieapp/src/components/MovieList.js
+++ React/movieapp/src/components/MovieList.js
@@ -19,11 +19,16 @@ export class MovieList extends Component {
 
 
         <div className="movies-list">
-              <ul>
-                     {movieData.map((movie)=>(
-                            <li><p>{movie.original_title}</p></li>
-                     ))}
-              </ul>
+              {movieData.map((movie)=>(
+                <div class="card movie-card">
+                <img src="https://static1.colliderimages.com/wordpress/wp-content/uploads/slice_inception_movie_poster_banner_01.jpg?q=50&fit=contain&w=1500&h=&dpr=1.5" class="card-img-top" alt="..."/>
+                <div class="card-body">
+                  <h5 class="card-title">Card title</h5>
+                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                  <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+              </div>
+              ))}
         </div>
       </>
     );
