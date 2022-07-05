38da0f8542e4333fa215a6e3a353c4ec066a15d2
mrinal1224 <iammrinalkc@gmail.com>
2022-07-05 22:14:34 +0530

    moviepage designed

--- React/movieapp/src/components/MovieList.js
+++ React/movieapp/src/components/MovieList.js
@@ -1,34 +1,65 @@
 import React, { Component } from "react";
 
-
-import {movies} from '../movieData'
+import { movies } from "../movieData";
 
 export class MovieList extends Component {
   render() {
-
-      let movieData = movies.results
-      console.log(movieData)
+    let movieData = movies.results;
+    console.log(movieData);
     return (
       <>
-      
         <div>
           <h3 className="text-center">
             <strong>Trending</strong>
           </h3>
         </div>
 
-
         <div className="movies-list">
-              {movieData.map((movie)=>(
-                <div class="card movie-card">
-                <img src="https://static1.colliderimages.com/wordpress/wp-content/uploads/slice_inception_movie_poster_banner_01.jpg?q=50&fit=contain&w=1500&h=&dpr=1.5" class="card-img-top" alt="..."/>
-                <div class="card-body">
-                  <h5 class="card-title">Card title</h5>
-                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                  <a href="#" class="btn btn-primary">Go somewhere</a>
-                </div>
-              </div>
-              ))}
+          {movieData.map((movieElem) => (
+            <div className="card movie-card">
+              <img
+                src={`https://image.tmdb.org/t/p/original${movieElem.backdrop_path}`}
+                className="card-img-top movie-img"
+                alt="..."
+                style={{ height: "40vh", width: "20vw" }}
+              />
+
+              <h5 class="card-title movie-title">{movieElem.original_title}</h5>
+              <a href="#" class="btn btn-primary movies-button">
+                Add to Favourites
+              </a>
+            </div>
+          ))}
+        </div>
+
+        <div style={{display : 'flex' , justifyContent : 'center'}}>
+          <nav aria-label="...">
+            <ul class="pagination">
+              <li class="page-item disabled">
+                <a class="page-link">Previous</a>
+              </li>
+              <li class="page-item">
+                <a class="page-link" href="#">
+                  1
+                </a>
+              </li>
+              <li class="page-item active" aria-current="page">
+                <a class="page-link" href="#">
+                  2
+                </a>
+              </li>
+              <li class="page-item">
+                <a class="page-link" href="#">
+                  3
+                </a>
+              </li>
+              <li class="page-item">
+                <a class="page-link" href="#">
+                  Next
+                </a>
+              </li>
+            </ul>
+          </nav>
         </div>
       </>
     );
