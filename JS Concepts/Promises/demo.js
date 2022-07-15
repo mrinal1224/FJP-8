49d9ae7611c82e0212a9220e8c12e76e574bb995
mrinal1224 <iammrinalkc@gmail.com>
2022-07-15 21:41:59 +0530

    promises

---
+++ JS Concepts/Promises/demo.js
@@ -0,0 +1,28 @@
+const fs = require('fs')
+
+
+// fs.readFile('f1.txt' , cb)
+
+// function cb(err , data){
+//     if(err){
+//        console.log(err)
+//     }
+//     console.log('File Data->  ' + data)
+// }
+
+
+
+let readFilePromise = fs.promises.readFile('f2.txt') // pending
+
+console.log(readFilePromise)
+
+
+// Fulfilled State
+readFilePromise.then(function(data){
+       console.log('This is file Data-> ' + data)
+})
+
+// Rejected
+readFilePromise.catch(function(err){
+       console.log(err)
+})
