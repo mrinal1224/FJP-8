e88c6d58140f473cb5c363302cdfe337047356fd
mrinal1224 <iammrinalkc@gmail.com>
2022-07-16 22:09:45 +0530

    edit

--- JS Concepts/Promises/asyncAwait.js
+++ JS Concepts/Promises/asyncAwait.js
@@ -1,30 +1,28 @@
-function placeOrder(drink){
-       return new Promise(function(resolve , reject){
-              if(drink==='coffee'){
-                     resolve('Order for coffee Placed')
-              }
-              else{
-                     reject('Order cannot be placed')
-              }
-       })
+function placeOrder(drink) {
+  return new Promise(function (resolve, reject) {
+    if (drink === "coffee") {
+      resolve("Order for coffee Placed");
+    } else {
+      reject("Order cannot be placed");
+    }
+  });
 }
 
-
-
-function processOrder(order){
-       return new Promise(function(resolve){
-              resolve(`${order} and Served`)
-       })
+function processOrder(order) {
+  return new Promise(function (resolve) {
+    resolve(`${order} and Served`);
+  });
 }
 
-
-
-placeOrder('coffee').then(function(orderStatus){
-       console.log(orderStatus)
-       let orderIsProcessed = processOrder(orderStatus)
-       return orderIsProcessed
-}).then(function(servedOrder){
-      console.log(servedOrder)
-}).catch(function(orderStatus){
-       console.log(orderStatus)
-})
+placeOrder("coffee")
+  .then(function (orderStatus) {
+    console.log(orderStatus);
+    let orderIsProcessed = processOrder(orderStatus);
+    return orderIsProcessed;
+  })
+  .then(function (servedOrder) {
+    console.log(servedOrder);
+  })
+  .catch(function (orderStatus) {
+    console.log(orderStatus);
+  });
