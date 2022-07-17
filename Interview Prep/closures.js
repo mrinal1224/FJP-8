417a394ab7a16d95945e3908c9aa70327c15ec1f
mrinal1224 <iammrinalkc@gmail.com>
2022-07-17 22:55:15 +0530

    closures question

--- Interview Prep/closures.js
+++ Interview Prep/closures.js
@@ -1,24 +1,89 @@
-function greet() {
-  let name = "Aman";
+// function greet() {
+//   let name = "Aman";
 
-  function displayName() {
-    console.log("Hi" + " " + name);
-    let age = 20;
+//   function displayName() {
+//     console.log("Hi" + " " + name);
+//     let age = 20;
 
-    function showAge() {
-      console.log(name + " " + age);
-    }
+//     function showAge() {
+//       console.log(name + " " + age);
+//     }
 
-    return showAge;
-  }
+//     return showAge;
+//   }
 
-  return displayName;
-}
+//   return displayName;
+// }
 
-let g1 = greet();
+// let g1 = greet();
 
-console.log(g1());
+// console.log(g1());
 
-let g2 = g1()
+// let g2 = g1()
 
-g2()
+// g2()
+
+
+// Questions ->
+
+
+1
+function createIncrement() {
+       let count = 0;
+       function increment() { 
+         count++;
+         console.log(count)
+       }
+       let message = `Count is ${count}`;
+       function log() {
+         console.log(message);
+       }
+       
+       return [increment, log];
+     }
+     const [increment, log] = createIncrement();
+     increment(); 
+     increment(); 
+     increment(); 
+     log(); // What is logged?
+
+
+
+//2
+
+
+function createStack() {
+       const items = []
+       return {
+         
+         push(item) {
+           items.push(item);
+         },
+         pop() {
+           return items.pop();
+         }
+       };
+     }
+     const stack = createStack();
+     stack.push(10)
+
+     stack.push(5);
+     
+     console.log(stack.pop()); // => 5
+
+
+
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
