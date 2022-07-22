46ae70a8deb8fd179fc9df7bdca200d2ccd057d2
mrinal1224 <iammrinalkc@gmail.com>
2022-07-22 23:11:20 +0530

    favourites \

--- React/movieapp/src/components/Favourites.js
+++ React/movieapp/src/components/Favourites.js
@@ -1,11 +1,75 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+
+import {movies} from '../movieData'
 
 export class Favourites extends Component {
   render() {
+    let genreids = {
+      28: "Action",
+      12: "Adventure",
+      16: "Animation",
+      35: "Comedy",
+      80: "Crime",
+      99: "Documentary",
+      18: "Drama",
+      10751: "Family",
+      14: "Fantasy",
+      36: "History",
+      27: "Horror",
+      10402: "Music",
+      9648: "Mystery",
+      10749: "Romance",
+      878: "Sci-Fi",
+      10770: "TV",
+      53: "Thriller",
+      10752: "War",
+      37: "Western",
+    };
+
+
+    let movieData = movies.results
+
     return (
-      <div><h1>I am Favorites Page</h1></div>
-    )
+      <div className="main">
+        <div className="row">
+          <div className="col-3">
+            <ul class="list-group list-group-flush">
+              <li class="list-group-item">Action</li>
+              <li class="list-group-item">Comedy</li>
+              <li class="list-group-item">Romance</li>
+              <li class="list-group-item">Fantasy</li>
+              <li class="list-group-item">Animation</li>
+            </ul>
+          </div>
+
+          <div className="col-9 favourites-table">
+            <table class="table">
+              <thead>
+                <tr>
+                  <th scope="col">Title</th>
+                  <th scope="col">Genre</th>
+                  <th scope="col">Popularity</th>
+                  <th scope="col">Ratings</th>
+                </tr>
+              </thead>
+              <tbody>
+
+              {movieData.map((movieObj)=>(
+                      <tr>
+                      <th scope="row">{movieObj.title}</th>
+                      <td>{genreids[movieObj.genre_ids[0]]}</td>
+                      <td>{movieObj.popularity}</td>
+                      <td>{movieObj.vote_average}</td>
+                    </tr>
+              ))}
+               
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
-export default Favourites
+export default Favourites;
--- React/movieapp/src/components/Navbar.js
+++ React/movieapp/src/components/Navbar.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react'
 
+
+import {Link} from 'react-router-dom'
+
 export class Navbar extends Component {
   render() {
     return (
       <div style={{display : 'flex' , alignItems:'center'}}>
-        <h1>Movies App</h1>
-        <h2 style={{marginLeft:"2rem" , marginTop :'0.7rem'}}>Favourites</h2>
+        <Link to='/'> <h1>Movies App</h1> </Link>
+        <Link to='favourites'><h2 style={{marginLeft:"2rem" , marginTop :'0.7rem'}}>Favourites</h2></Link>
       </div>
     )
   }
