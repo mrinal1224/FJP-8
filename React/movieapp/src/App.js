038dad6add61a1241eabbfe3e1f6b18c656d5be5
mrinal1224 <iammrinalkc@gmail.com>
2022-07-22 22:15:52 +0530

    react router

--- React/movieapp/src/App.js
+++ React/movieapp/src/App.js
@@ -1,17 +1,31 @@
-import './App.css'
+import "./App.css";
 
 import Banner from "./components/Banner";
-import MovieList from './components/MovieList';
+import Favourites from "./components/Favourites";
+import MovieList from "./components/MovieList";
 import Navbar from "./components/Navbar";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    
-   <>
-     <Navbar/>
-     <Banner/>
-     <MovieList/>
-   </>
+    <BrowserRouter>
+      <Navbar />
+
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <Banner />
+              <MovieList />
+            </>
+          }
+        />
+
+        <Route path="/favourites" element={<Favourites />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ React/movieapp/src/components/Favourites.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class Favourites extends Component {
+  render() {
+    return (
+      <div><h1>I am Favorites Page</h1></div>
+    )
+  }
+}
+
+export default Favourites
