ad36cd9d216229859c2216eeda227d786b51fc4e
mrinal1224 <iammrinalkc@gmail.com>
2022-07-23 23:42:54 +0530

    push

--- React/movieapp/src/components/Favourites.js
+++ React/movieapp/src/components/Favourites.js
@@ -9,6 +9,7 @@ export class Favourites extends Component {
     this.state = {
       genres: [],
       movies: [],
+      currGenre : 'All Genres'
     };
   }
 
@@ -45,10 +46,20 @@ export class Favourites extends Component {
       }
     });
 
+    tempArr.unshift('All Genres')
+
     this.setState({
       movies: [...data],
       genres: [...tempArr],
     });
+  }
+
+  handleGenreChange =(genre)=>{
+       this.setState({
+              currGenre : genre
+       })
+
+      
   }
 
   render() {
@@ -74,24 +85,49 @@ export class Favourites extends Component {
       37: "Western",
     };
 
-    let movieData = movies.results;
+   let filterArr =[]
+
+   if(this.state.currGenre='All genres'){
+       filterArr = this.state.movies
+   }
+
+
+   else{
+       filterArr = this.state.movies.filter((movieObj)=> genreids[movieObj.genre_ids[0]]==this.state.currGenre)
+   } 
+
+  
 
     return (
       <div className="main">
         <div className="row">
           <div className="col-3">
             <ul class="list-group genre-selector">
-              {this.state.genres.map((genre) => (
+              {this.state.genres.map((genre) => this.state.currGenre==genre?
+              (
                 <li
                   style={{
-                    background: "white",
-                    color: " #3f51b5",
+                    background: "#3f51b5",
+                    color: " white",
                     fontWeight: "bold",
                   }}
                   className="list-group-item"
                 >
                   {genre}
                 </li>
+              ) : (
+                     <li
+                     style={{
+                       background: "white",
+                       color: "#3f51b5",
+                       fontWeight: "bold",
+                     }}
+                     className="list-group-item"
+
+                     onClick={()=>this.handleGenreChange(genre)}
+                   >
+                     {genre}
+                   </li>
               ))}
             </ul>
           </div>
@@ -118,7 +154,7 @@ export class Favourites extends Component {
                   </tr>
                 </thead>
                 <tbody>
-                  {this.state.movies.map((movieObj) => (
+                  {filterArr.map((movieObj) => (
                     <tr>
                       <td>
                         <img
