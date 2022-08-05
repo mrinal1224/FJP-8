7ece29dfcf30104aba1e1a65aad77dad73304bc6
mrinal1224 <iammrinalkc@gmail.com>
2022-08-05 23:05:22 +0530

    push

--- React/demofirebase/src/components/FireAuth.js
+++ React/demofirebase/src/components/FireAuth.js
@@ -1,30 +1,71 @@
-import React , {useState} from 'react'
+import React, { useState, useEffect } from "react";
 
-import { auth } from '../firebase'
+import { auth } from "../firebase";
 
 const FireAuth = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [user, setUser] = useState("");
 
-       const [email , setEmail] = useState('')
-       const [password , setPassword] = useState('')
-       const [user , setUser] = useState('')
+  let create = async () => {
+    let userCreated = await auth.createUserWithEmailAndPassword(
+      email,
+      password
+    );
+    console.log(userCreated);
+  };
+
+  useEffect(() => {
+    let userSubscribed = auth.onAuthStateChanged((user) => {
+      setUser(user);
+    });
+
+    return () => {
+      userSubscribed(); // cleanUp
+    };
+  }, []);
 
 
-       const create = async ()=>{
-          let userCreated = await auth.createUserWithEmailAndPassword(email , password)
-          console.log(userCreated)
-       }
+  const logout = async ()=>{
+          await auth.signOut()
+  }
+
+  const login = async ()=>{
+       await auth.signInWithEmailAndPassword(email , password)
+  }
+
   return (
-    <div>
+    <>
+     {
 
-       <label>email</label>
-       <input type='text' value={email} onChange={(e)=>setEmail(e.target.value)}/>
-       <label>password</label>
-       <input type='password' value={password} onChange={(e)=>setPassword(e.target.value)}/>
+       user ==null?
 
-       <button onClick={create}>Sign Up</button>
-       <button>Log In</button>
-    </div>
-  )
+   
+      <div>
+        <label>email</label>
+        <input
+          type="text"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <label>password</label>
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <button onClick={create}>Sign Up</button>
+
+        <button onClick={login}>Log In</button>
+      </div> : 
+      
+      <div> <h1>{user.email}</h1>
+             <button onClick={logout}>Log Out</button>
+      </div>
 }
+    </>
+  );
+};
 
-export default FireAuth
+export default FireAuth;
