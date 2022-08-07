3157570b6b68b74e1d2604e920eacfd4a483fc8d
mrinal1224 <iammrinalkc@gmail.com>
2022-08-07 23:24:54 +0530

    push

--- React/reels/src/components/SignUp.js
+++ React/reels/src/components/SignUp.js
@@ -18,8 +18,17 @@ export default function SignUp() {
        const useStyles = createUseStyles({
               text1 : {
                      color : 'grey',
-                     textAlign : 'center'
+                     textAlign : 'center',
+                     marginTop : '3%'
 
+              },
+
+              buttonMargin : {
+                     marginTop : '4%'
+              },
+
+              card2:{
+                     marginTop : '5%'
               }
        })
 
@@ -45,16 +54,26 @@ export default function SignUp() {
             <TextField id="outlined-basic" label="Password" variant="outlined" margin='dense' fullWidth={true} size='small' />
             <TextField id="outlined-basic" label="Full Name" variant="outlined" margin='dense' fullWidth={true} size='small' />
 
-            <Button color="secondary" variant='outlined' fullWidth={true} component='label' >Upload Profile Picture
+            <Button color="secondary" variant='outlined' fullWidth={true} component='label' className={classes.buttonMargin} >Upload Profile Picture
              <input type='file' accept="image/*" hidden/>
             
             </Button>
+
+            <Typography variant='subtitle1' className={classes.text1} margin="dense">
+            By signing up, you agree to our Terms , Privacy Policy and Cookies Policy .
+            </Typography>
            
           </CardContent>
           <CardActions>
-            <Button size="small">Share</Button>
-            <Button size="small">Learn More</Button>
+         
+            <Button  variant="contained" fullWidth={true}>Sign Up</Button>
           </CardActions>
+        </Card>
+
+        <Card sx={{ maxWidth: 345 }} className={classes.card2}>
+              <Typography>
+                     Have an Account? Log in
+              </Typography>
         </Card>
       </div>
     </div>
