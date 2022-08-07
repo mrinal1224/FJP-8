b8fba0696997d8703329a6f442170ea3b302332a
mrinal1224 <iammrinalkc@gmail.com>
2022-08-07 22:04:41 +0530

    reels

---
+++ React/reels/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import SignUp from './components/SignUp';
+
+function App() {
+  return (
+    <div className="App">
+     <SignUp/>
+    </div>
+  );
+}
+
+export default App;
