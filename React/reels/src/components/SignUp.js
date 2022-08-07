c60f490dbb8dfdea46d8282dd66ba67247bb98c4
mrinal1224 <iammrinalkc@gmail.com>
2022-08-07 22:31:55 +0530

    push

--- React/reels/src/components/SignUp.js
+++ React/reels/src/components/SignUp.js
@@ -2,26 +2,39 @@ import * as React from "react";
 import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
+
+import {createUseStyles} from 'react-jss'
+
+import instaLogo from '../Assets/Instagram.JPG'
 
 import './signup.css'
 
 export default function SignUp() {
+
+       const useStyles = createUseStyles({
+              text1 : {
+                     color : 'grey',
+
+              }
+       })
+
+       const classes = useStyles()
+
+
   return (
     <div className="signupWrapper">
       <div className='signupCard'>
         <Card sx={{ maxWidth: 345 }}>
-          <CardMedia
-            component="img"
-            height="140"
-            image="/static/images/cards/contemplative-reptile.jpg"
-            alt="green iguana"
-          />
+
+              <div className='insta-logo'>
+                     <img src={instaLogo}/>
+              </div>
+          
           <CardContent>
-            <Typography gutterBottom variant="h5" component="div">
-              Lizard
+            <Typography variant='subtitle1' className={classes.text1}>
+              Sign up to see photos and videos from your friends
             </Typography>
             <Typography variant="body2" color="text.secondary">
               Lizards are a widespread group of squamate reptiles, with over
