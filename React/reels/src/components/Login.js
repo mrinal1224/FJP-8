eb445c3584bc237a97b4538ebcab41398ffc49f6
mrinal1224 <iammrinalkc@gmail.com>
2022-08-11 22:12:30 +0530

    push

--- React/reels/src/components/Login.js
+++ React/reels/src/components/Login.js
@@ -4,113 +4,134 @@ import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
-import Alert from '@mui/material/Alert';
-import TextField from '@mui/material/TextField';
+import Alert from "@mui/material/Alert";
+import TextField from "@mui/material/TextField";
 
-import {createUseStyles} from 'react-jss'
+import { createUseStyles } from "react-jss";
 
-import instaLogo from '../Assets/Instagram.JPG'
+import instaLogo from "../Assets/Instagram.JPG";
+import insta from "../Assets/insta.png";
 
-import './login.css'
+import img1 from "../Assets/img1.jpg";
+import img2 from "../Assets/img2.jpg";
+import img3 from "../Assets/img3.jpg";
+import img4 from "../Assets/img4.jpg";
+import img5 from "../Assets/img5.jpg";
+
+import { CarouselProvider, Slider, Slide, Image } from "pure-react-carousel";
+import "pure-react-carousel/dist/react-carousel.es.css";
+
+import "./login.css";
 
 export default function SignUp() {
+  const useStyles = createUseStyles({
+    text1: {
+      color: "grey",
+      textAlign: "center",
+      marginTop: "3%",
+    },
 
-       const useStyles = createUseStyles({
-              text1 : {
-                     color : 'grey',
-                     textAlign : 'center',
-                     marginTop : '3%'
+    text2: {
+      color: "#0095f6",
+    },
 
-              },
+    buttonMargin: {
+      marginTop: "4%",
+    },
 
-              text2 :{
-                     color : '#0095f6'
-              },
+    card2: {
+      marginTop: "5%",
+    },
+  });
 
-              buttonMargin : {
-                     marginTop : '4%'
-              },
-
-              card2:{
-                     marginTop : '5%'
-              }
-       })
-
-       const classes = useStyles()
-
+  const classes = useStyles();
 
   return (
     <div className="loginWrapper">
-      <div className='loginCard'>
-        <Card sx={{ maxWidth: 345 }}>
-
-              <div className='insta-logo'>
-                     <img src={instaLogo}/>
-              </div>
-          
-          <CardContent>
-            <Typography variant='subtitle1' className={classes.text1}>
-              Sign up to see photos and videos from your friends
-            </Typography>
-            {true && <Alert severity="error">This is an error alert — check it out!</Alert>}
-
-            <TextField id="outlined-basic" label="Email" variant="outlined" margin='dense' fullWidth={true} size='small' />
-            <TextField id="outlined-basic" label="Password" variant="outlined" margin='dense' fullWidth={true} size='small' />
-            <TextField id="outlined-basic" label="Full Name" variant="outlined" margin='dense' fullWidth={true} size='small' />
-
-            <Button color="secondary" variant='outlined' fullWidth={true} component='label' className={classes.buttonMargin} >Upload Profile Picture
-             <input type='file' accept="image/*" hidden/>
-            
-            </Button>
-
-            <Typography variant='subtitle1' className={classes.text1} margin="dense">
-            By signing up, you agree to our Terms , Privacy Policy and Cookies Policy .
-            </Typography>
-           
-          </CardContent>
-          <CardActions>
-         
-            <Button  variant="contained" fullWidth={true}>Sign Up</Button>
-          </CardActions>
-        </Card>
-
-        <Card sx={{ maxWidth: 345 }} className={classes.card2}>
-              <Typography>
-                     Have an Account? Log in
-              </Typography>
-        </Card>
+      <div
+        className="img-car"
+        style={{ backgroundImage: "url(" + insta + ")" , backgroundSize:'cover' }}
+      >
+        <div className="car">
+          <CarouselProvider
+               visibleSlides={1}
+               totalSlides={5}
+               // step={3}
+               naturalSlideWidth={238}
+               naturalSlideHeight={423}
+               hasMasterSpinner
+               isPlaying={true}
+               infinite={true}
+               dragEnabled={false}
+               touchEnabled={false}
+          >
+            <Slider>
+              <Slide index={0}>
+                <Image src={img1} />
+              </Slide>
+              <Slide index={1}>
+                <Image src={img2} />
+              </Slide>
+              <Slide index={2}>
+                <Image src={img3} />
+              </Slide>
+              <Slide index={3}>
+                <Image src={img4} />
+              </Slide>
+              <Slide index={4}>
+                <Image src={img5} />
+              </Slide>
+            </Slider>
+          </CarouselProvider>
+        </div>
       </div>
 
-      <div className='signupCard'>
+      <div className="signupCard">
         <Card sx={{ maxWidth: 345 }}>
+          <div className="insta-logo">
+            <img src={instaLogo} />
+          </div>
 
-              <div className='insta-logo'>
-                     <img src={instaLogo}/>
-              </div>
-          
           <CardContent>
-            
-            {true && <Alert severity="error">This is an error alert — check it out!</Alert>}
+            {true && (
+              <Alert severity="error">
+                This is an error alert — check it out!
+              </Alert>
+            )}
 
-            <TextField id="outlined-basic" label="Email" variant="outlined" margin='dense' fullWidth={true} size='small' />
-            <TextField id="outlined-basic" label="Password" variant="outlined" margin='dense' fullWidth={true} size='small' />
-            <Typography variant='subtitle1' className={classes.text2} margin="dense">
-                Forgot Password?
+            <TextField
+              id="outlined-basic"
+              label="Email"
+              variant="outlined"
+              margin="dense"
+              fullWidth={true}
+              size="small"
+            />
+            <TextField
+              id="outlined-basic"
+              label="Password"
+              variant="outlined"
+              margin="dense"
+              fullWidth={true}
+              size="small"
+            />
+            <Typography
+              variant="subtitle1"
+              className={classes.text2}
+              margin="dense"
+            >
+              Forgot Password?
             </Typography>
-            
-           
-           
           </CardContent>
           <CardActions>
-         
-            <Button  variant="contained" fullWidth={true}>Log in</Button>
+            <Button variant="contained" fullWidth={true}>
+              Log in
+            </Button>
           </CardActions>
         </Card>
 
         <Card sx={{ maxWidth: 345 }} className={classes.card2}>
-              <Typography>
-                     New User? Sign up
-              </Typography>
+          <Typography>New User? Sign up</Typography>
         </Card>
       </div>
     </div>
