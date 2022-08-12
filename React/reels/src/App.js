a2cb48288a8a6bbf3938fc15fcc56a1d9f8c9c12
mrinal1224 <iammrinalkc@gmail.com>
2022-08-12 22:02:51 +0530

    push

--- React/reels/src/App.js
+++ React/reels/src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import SignUp from './components/SignUp';
 import Login from "./components/Login"
 
+import { AuthProvider } from './Context/AuthContext';
+
 
 import {BrowserRouter , Routes , Route} from 'react-router-dom'
 
@@ -11,11 +13,13 @@ function App() {
     <div className="App">
 
       <BrowserRouter>
+      <AuthProvider>
        <Routes>
 
         <Route path='/' element={<SignUp/>}/>
         <Route path='/login' element={<Login/>}/>
        </Routes>
+       </AuthProvider>
       </BrowserRouter>
   
      
---
+++ React/reels/src/Context/AuthContext.js
@@ -0,0 +1,54 @@
+import React , {useState , useEffect} from 'react'
+
+import {auth} from '../firebase'
+
+const AuthContext = React.createContext()
+
+
+export function AuthProvider({children}){
+
+
+         const [user , setUser] = useState()
+         const [loading , setLoading] = useState(true)
+
+         
+
+       function signup(email , password){
+              return auth.createUserWithEmailAndPassword(email , password)
+       }
+
+
+       function login(email , password){
+              return auth.signInWithEmailAndPassword(email , password)
+       }
+
+       function logout(){
+              return auth.signOut()
+       }
+
+         useEffect(()=>{
+                const unSub = auth.onAuthStateChanged((user)=>{
+                     setUser(user)
+                     setLoading(false)
+                })
+
+                return ()=>{
+                     unSub()
+                }
+         } , [])
+
+
+         const store = {
+              user,
+              signup,
+              login,
+              logout
+         }
+
+         return(
+              <AuthContext.Provider value={store}>
+                   {!loading && children}
+              </AuthContext.Provider>
+         )
+
+}
