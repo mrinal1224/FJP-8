a792c2599f3dbb470bc5060d4c81f52e15909cd9
mrinal1224 <iammrinalkc@gmail.com>
2022-09-13 22:32:55 +0530

    push

--- React/reels/src/components/Posts.js
+++ React/reels/src/components/Posts.js
@@ -1,54 +1,86 @@
-import React, { useState,useEffect } from 'react'
-import {database} from '../firebase';
-import CircularProgress from '@mui/material/CircularProgress';
-import Video from './Video'
-import Avatar from '@mui/material/Avatar';
-import './Post.css'
-import Like from './Like';
+import React, { useState, useEffect } from "react";
+import { database } from "../firebase";
+import CircularProgress from "@mui/material/CircularProgress";
+import Video from "./Video";
+import Avatar from "@mui/material/Avatar";
+import ChatBubbleIcon from "@mui/icons-material/ChatBubble";
+import Dialog from "@mui/material/Dialog";
+import "./Post.css";
+import Like from "./Like";
+import Comment from "./Comment";
 
+function Posts({ userData }) {
+  const [posts, setPosts] = useState(null);
 
-function Posts({userData}) {
-    const [posts,setPosts] = useState(null);
-   
-    useEffect(()=>{
-        let parr = []
-        const unsub = database.posts.orderBy('createdAt','desc').onSnapshot((querySnapshot)=>{
-            parr = []
-            querySnapshot.forEach((doc)=>{
-                let data = {...doc.data(),postId:doc.id}
-                parr.push(data)
-            })
-            setPosts(parr)
-        })
-        return unsub
-    },[])
- 
-    return (
-        <div>
-            {
-                posts==null || userData==null ? <CircularProgress /> :
-                <div className="video-container">
-                    {
-                        posts.map((post,index)=>(
-                            <React.Fragment key={index}>
-                                {console.log(post)}
-                                <div className="videos">
-                                    <Video src={post.pUrl} id={post.pId}/>
-                                    <div className='fa'style={{display:'flex'}}>
-                                    <Avatar src={post.uProfile}/>
-                                     <h4>{post.uName}</h4>
-                                     <Like userData={userData}  postData={post} />
-                                    </div>
-                                    
-                                    
-                                </div>
-                            </React.Fragment>
-                        ))
-                    }
+  const [open, setOpen] = useState(null);
+
+  const handleClickOpen = (id) => {
+    setOpen(id);
+  };
+
+  const handleClose = () => {
+    setOpen(null);
+  };
+
+  useEffect(() => {
+    let parr = [];
+    const unsub = database.posts
+      .orderBy("createdAt", "desc")
+      .onSnapshot((querySnapshot) => {
+        parr = [];
+        querySnapshot.forEach((doc) => {
+          let data = { ...doc.data(), postId: doc.id };
+          parr.push(data);
+        });
+        setPosts(parr);
+      });
+    return unsub;
+  }, []);
+
+  return (
+    <div>
+      {posts == null || userData == null ? (
+        <CircularProgress />
+      ) : (
+        <div className="video-container">
+          {posts.map((post, index) => (
+            <React.Fragment key={index}>
+              {console.log(post)}
+              <div className="videos">
+                <Video src={post.pUrl} id={post.pId} />
+                <div className="fa" style={{ display: "flex" }}>
+                  <Avatar src={post.uProfile} />
+                  <h4>{post.uName}</h4>
+                  <Like userData={userData} postData={post} />
                 </div>
-            }
+                <ChatBubbleIcon
+                  className="chat-styling"
+                  onClick={() => handleClickOpen(post.pId)}
+                />
+
+                <Dialog
+                  open={open == post.pId}
+                  onClose={handleClose}
+                  aria-labelledby="alert-dialog-title"
+                  aria-describedby="alert-dialog-description"
+                  fullWidth={true}
+                  maxWidth="md"
+                >
+                  <div className="modal-container">
+                    <div className="video-modal">
+                      <video autoPlay={true} muted="muted" controls>
+                        <source src={post.pUrl} />
+                      </video>
+                    </div>
+                  </div>
+                </Dialog>
+              </div>
+            </React.Fragment>
+          ))}
         </div>
-    )
+      )}
+    </div>
+  );
 }
 
-export default Posts
+export default Posts;
