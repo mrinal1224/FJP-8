156102fd1111fb3c294a240c46ed3b1bc4d279f1
mrinal1224 <iammrinalkc@gmail.com>
2022-09-13 22:07:50 +0530

    like implemented

--- React/reels/src/components/Like.js
+++ React/reels/src/components/Like.js
@@ -1,17 +1,40 @@
-import React , {useState} from 'react'
+import React , {useEffect, useState} from 'react'
+import { database } from '../firebase';
 
 import FavoriteIcon from '@mui/icons-material/Favorite';
 
 function Like({userData , postData}) {
 
        const[like , setLike] = useState(true)
+
+       useEffect(()=>{
+            
+            let check = postData.likes.includes(userData.userId)?true : false
+            setLike(check)
+       } , [postData])
+
+
+       const handleLike =()=>{
+              if(like == true){
+                     let narr = postData.likes.filter((el)=>el!=userData.userId)
+                     database.posts.doc(postData.postId).update({
+                            likes : narr
+                     })
+              }
+              else{
+                     let narr = [...postData.likes , userData.userId]
+                     database.posts.doc(postData.postId).update({
+                            likes : narr
+                     })
+              }
+       }
   return (
       <div>
        {
               like !=null?
               <>
                    {
-                     like==true? <FavoriteIcon className={`icon-styling like`} /> : <FavoriteIcon className={`icon-styling unlike`}/>
+                     like==true? <FavoriteIcon className={`icon-styling like`} onClick={handleLike} /> : <FavoriteIcon className={`icon-styling unlike`} onClick={handleLike}/>
                    }   
               </> :
               <>
